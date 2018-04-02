a44cf483816cf2dfc57ed00193f0567624cc595e
AnnaFlourou <flourou.anna@gmail.com>
2018-04-02 16:58:56 +0200

    add exercices adv and math

--- exercises/jaden-case.js
+++ exercises/jaden-case.js
@@ -7,11 +7,23 @@
  * and return capitalize each words: "How are you ?" -> "How Are You ?"
  *
  */
-
-
+function jadenCase(word) {
+    let sentence = word.toLowerCase();
+    let tab = sentence.split(' ');
+    let temp = "";
+    for (let i = 0; i < tab.length; i++) {
+        tab[i] = tab[i][0].toUpperCase() + tab[i].slice(1);
+        temp = tab.join(' ');
+    }
+    return temp
+}
 
 //* Begin of tests
 const assert = require('assert')
+jadenCase("Parce que c'est notre projet");
+jadenCase("le lion ne s'associe pas avec le cafard");
+assert.strictEqual(jadenCase("hello world"), "Hello World")
+assert.strictEqual(jadenCase("to be or not to be"), "To Be Or Not To Be")
 
-assert.fail('You must write your own tests')
-// End of tests */
+
+
